d0dc355c4afc4a1ce178e3237b54dce5e101973a
Jacqueline <ojedaaa11@gmail.com>
2022-06-07 20:02:28 -0300

    registro listo

--- app/Views/back/registrarse.php
+++ app/Views/back/registrarse.php
@@ -1,69 +1,80 @@
-
-<div class="container mt-1 mb-0">
-	<div class="card" style="width: 50%;" >
-		<div class= "card-header text-center">
-			<h2>Agregar usuarios</h2>
-		</div>
-	
- <?php $validation = \Config\Services::validation(); ?>
-     <form method="post" action="<?php echo base_url('enviar-form') ?>">
- 
-<div class ="card-body" media="(max-width:768px)">
-	<div class="mb-2">
- 	 <label for="exampleFormControlInput1" class="form-label">Nombre</label>
- 	 <input name="nombre" type="text"  class="form-control" placeholder="nombre" >
-     <!-- Error -->
-        <?php if($validation->getError('nombre')) {?>
-            <div class='alert alert-danger mt-2'>
-              <?= $error = $validation->getError('nombre'); ?>
-            </div>
-        <?php }?>
-	</div>
-	<div class="mb-3">
- 	 <label for="exampleFormControlTextarea1" class="form-label">Apellido</label>
- 	  <input type="text" name="apellido"class="form-control" placeholder="apellido" >
- 	  <!-- Error -->
-        <?php if($validation->getError('apellido')) {?>
-            <div class='alert alert-danger mt-2'>
-              <?= $error = $validation->getError('apellido'); ?>
-            </div>
-        <?php }?>
+<br>
+<br>
+<div class="container">
+  <div class="row no-gutters" style="background-color:white ; border-radius:30px; box-shadow: 12px 12px 22px grey">
+    <!-- FOTO -->
+    <div class=" col-lg-5 mt-4">
+      <img src="public\assets\img\musica.jpg" class="img-fluid" alt="" style="border-radius:30px;">
     </div>
-    <div class="mb-3">
-    	 <label for="exampleFormControlInput1" class="form-label">email</label>
- 	 <input name="email"  type="femail" class="form-control"  placeholder="correo@algo.com" >
- 	  <!-- Error -->
-        <?php if($validation->getError('email')) {?>
-            <div class='alert alert-danger mt-2'>
-              <?= $error = $validation->getError('email'); ?>
-            </div>
-        <?php }?>
-	</div>
- 	  <div class="mb-3">
- 	<label for="exampleFormControlInput1" class="form-label">Usuario</label>
- 	 <input  tyupe="text" name="usuario" class="form-control" placeholder="usuario">
- 	 <!-- Error -->
-        <?php if($validation->getError('usuario')) {?>
-            <div class='alert alert-danger mt-2'>
-              <?= $error = $validation->getError('usuario'); ?>
-            </div>
-        <?php }?>
-	</div>
-	
-	<div class="mb-3">
- 	 <label for="exampleFormControlInput1" class="form-label">Password</label>
- 	 <input name="password" type="password" class="form-control"  placeholder="password">
- 	 <!-- Error -->
-        <?php if($validation->getError('password')) {?>
-            <div class='alert alert-danger mt-2'>
-              <?= $error = $validation->getError('password'); ?>
-            </div>
-        <?php }?>
- 	</div>
- 	         <input type="submit" value="guardar" class="btn btn-success">
- 	          <input type="reset" value="cancelar" class="btn btn-danger">
- 	    
- </div>
-</form>
+    <!-- FOTO -->
+    <!-- TABLA REGISTRO -->
+    <div class=" col-lg-7 px-5 pt-5" style="width: 50%;">
+      <div class="text-center col-10">
+        <h1>REGISTRARSE</h1>
+      </div>
+      <?php $validation = \Config\Services::validation(); ?>
+      <form method="post" action="<?php echo base_url('enviar-form') ?>">
+        <div class="card-body" media="(max-width:768px)">
+          <div class="mb-2">
+            <label for="exampleFormControlInput1" class="form-label" style="color:darkgreen ;">Nombre</label>
+            <input name="nombre" type="text" class="form-control" placeholder="nombre">
+            <!-- Error -->
+            <?php if ($validation->getError('nombre')) { ?>
+              <div class='alert alert-danger mt-2'>
+                <?= $error = $validation->getError('nombre'); ?>
+              </div>
+            <?php } ?>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label" style="color:darkgreen ;">Apellido</label>
+            <input type="text" name="apellido" class="form-control" placeholder="apellido">
+            <!-- Error -->
+            <?php if ($validation->getError('apellido')) { ?>
+              <div class='alert alert-danger mt-2'>
+                <?= $error = $validation->getError('apellido'); ?>
+              </div>
+            <?php } ?>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label" style="color:darkgreen ;">Correo electrónico</label>
+            <input name="email" type="femail" class="form-control" placeholder="ejemplo@gmail.com">
+            <!-- Error -->
+            <?php if ($validation->getError('email')) { ?>
+              <div class='alert alert-danger mt-2'>
+                <?= $error = $validation->getError('email'); ?>
+              </div>
+            <?php } ?>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label" style="color:darkgreen ;">Usuario</label>
+            <input tyupe="text" name="usuario" class="form-control" placeholder="usuario">
+            <!-- Error -->
+            <?php if ($validation->getError('usuario')) { ?>
+              <div class='alert alert-danger mt-2'>
+                <?= $error = $validation->getError('usuario'); ?>
+              </div>
+            <?php } ?>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label" style="color:darkgreen ;">Contraseña</label>
+            <input name="password" type="password" class="form-control" placeholder="contraseña">
+            <!-- Error -->
+            <?php if ($validation->getError('password')) { ?>
+              <div class='alert alert-danger mt-2'>
+                <?= $error = $validation->getError('password'); ?>
+              </div>
+            <?php } ?>
+          </div>
+          <input type="submit" value="guardar" class="btn btn-success">
+          <input type="reset" value="cancelar" class="btn btn-danger">
+        </div>
+      </form>
+      <div class="d-grid">
+        <a class="btn btn-link" href="<?php echo base_url('login'); ?>">¿ ESTAS REGISTRADO?HACE CLIC AQUÍ</a>
+      </div>
+       <!-- TABLA REGISTRO -->
+    </div>
+  </div>
 </div>
-</div>
+<br>
+<br>
